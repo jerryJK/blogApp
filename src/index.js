4c0946107a7efc5461bd77b90ce7a56f8dabcc88
jerryJK <jaroslawkuliga@gmail.com>
2017-11-23 09:39:04 +0100

    add PostIndex component, set up reducer, add fetchPosts action

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
+import {BrowserRouter, Route } from 'react-router-dom';
+import PostIndex from './components/post_index';
 import reducers from './reducers';
+import promise from 'redux-promise';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <Route path="/" component={PostIndex} />
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
